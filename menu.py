d6032b2293e9d2fde222f09804d2ad32d153b73e
Ralph Ghannam <43726641+ghannamr@users.noreply.github.com>
2019-07-24 15:48:18 -0400

    Add files via upload
    text on planets

--- menu.py
+++ menu.py
@@ -8,28 +8,14 @@ from setup_master_database import setup_master_database
 setup_master_database()
 list_of_planets = load_planet_database()
 list_of_planet_models = []
+list_of_planet_text = []
 
-def Jacob(time_increment):
-    '''
-    --------------------------------------------------------------------------------
-    This is a function to test the GUI:
-    - Input the time to simulate the orbit: works well inside a loop when incrementing the time variable
-    - Outputs a list containing the earth and mars's 3d coordinates
-    - The distances are in Km, which means very large values (we will porbably be moving to astronomical units (AU) later for that reason).
-    - The angles are in radians as requested
-    --------------------------------------------------------------------------------
-    '''
-    # Earth = planet(5.972 * math.pow(10, 24), 149598262.00, 0.017, 147098291.00, 365.25636, 0, -0.196535244, 1.796767421, 1.749518042, Sun)
-    # Mars = planet(5.972 * math.pow(10, 24), 227943823.5, 0.093, 206655215, 687.0106875, 0.032288591, 0.865308761, 5.865019079, 0.7309438907, Sun)
-    list_of_planets[0].simulate_orbit(time_increment)
-    list_of_planets[1].simulate_orbit(time_increment)
-    results = [list_of_planets[1].xPos, list_of_planets[1].yPos, list_of_planets[1].zPos, list_of_planets[0].xPos, list_of_planets[0].yPos, list_of_planets[0].zPos]
-    return results
 
 def simulate_next_planet_iteration(t): #simulates all the planets for an increment t
     for planet in list_of_planets:
         planet.simulate_orbit(t)
         list_of_planet_models[list_of_planets.index(planet)].pos = vp.vector(planet.xPos / 10000000, planet.yPos / 10000000, planet.zPos / 10000000)
+        list_of_planet_text[list_of_planets.index(planet)].pos = list_of_planet_models[list_of_planets.index(planet)].pos + list_of_planet_models[list_of_planets.index(planet)].axis
 
 def display():
     vp.scene.append_to_title('\n')
@@ -40,7 +26,6 @@ def display():
     vp.scene.append_to_caption('\n')
     t = vp.slider(min=-20000, max=20000, value=1, length=675, bind=setyear)
     vp.scene.append_to_caption('\n')
-    celestpos = Jacob(t.value)
     # makes the sun shine
     vp.sphere(color=vp.color.yellow, emissive=True)
     vp.local_light(pos=vp.vector(0, 0, 0), color=vp.color.yellow)
@@ -51,6 +36,7 @@ def display():
         list_of_planet_models.append(vp.sphere(textures=vp.textures.earth,
                          pos=vp.vector(planet.xPos / 10000000, planet.yPos / 10000000, planet.zPos / 10000000),
                          make_trail=True, trail_type="points", interval=10, retain=25))
+        list_of_planet_text.append(vp.text(text = planet.name, pos = list_of_planet_models[list_of_planets.index(planet)].pos + list_of_planet_models[list_of_planets.index(planet)].axis, height = 0.5, color = vp.color.white, billboard = True, emissive = True))
     ############################################################################
     """
     # creates the visual representation of the planets
@@ -91,27 +77,6 @@ def display():
                 current_date -= date_increment
                 t.value -= 1
                 time.sleep(.01)
-            print(current_date)
-
-def display_at(date):
-    # insert planets
-    Sun = star(1.989 * math.pow(10, 30))
-    Earth = planet(5.972 * math.pow(10, 24), 149598262.00, 0.017, 147098291.00, 365.25636, Sun)
-    Venus = planet(5.972 * math.pow(10, 24), 108209475.00, 0.007, 107476170.00, 224.7057127, Sun)
-    Mercury = planet(5.972 * math.pow(10, 24), 57909227.00, 0.206, 46001009.00, 87.95373149, Sun)
-    #    Moon = satellite(7.34767 * math.pow(10, 22), 384400, Earth)
-    celestpos = Jacob(date,list_of_planets)
-
-    # makes the sun shine,
-    vp.sphere(color=vp.color.yellow, emissive=True)
-    vp.local_light(pos=vp.vector(0, 0, 0), color=vp.color.yellow)
-    # creates the visual representation of the planets
-    earth = vp.sphere(texture=vp.textures.earth,
-                      pos=vp.vector(celestpos[0] / 10000000, celestpos[1] / 10000000, celestpos[2] / 10000000),
-                      make_trail=True, trail_type="points", interval=10, retain=25)
-    mars = vp.sphere(color=vp.vector(1, 0, 0),
-                     pos=vp.vector(celestpos[3] / 10000000, celestpos[4] / 10000000, celestpos[5] / 10000000),
-                     make_trail=True, trail_type="points", interval=10, retain=25)
 
 
 def showat():
@@ -151,16 +116,6 @@ def Play():
     global pause
     pause  = False
 
-def credits():
-    t0 = vp.text(text='Jacob Jones', pos=vp.vec(5,5,0),color=vp.color.cyan, billboard=True, emissive=True)
-    t1 = vp.text(text='Ralph Ghannam', pos=vp.vec(3,3,0),color=vp.color.cyan, billboard=True, emissive=True)
-    t2 = vp.text(text='Matthew Cosman', pos=vp.vec(1,1,0),color=vp.color.cyan, billboard=True, emissive=True)
-    t3 = vp.text(text='Ameer Noufal', pos=vp.vec(-1,-1,0),color=vp.color.cyan, billboard=True, emissive=True)
-    t4 = vp.text(text='Akshar Patel', pos=vp.vec(-3,-3,0),color=vp.color.cyan, billboard=True, emissive=True)
-    t5 = vp.text(text='Daniel Smith De-Paz', pos=vp.vec(-5,-5,0),color=vp.color.cyan, billboard=True, emissive=True)
-    t6 = vp.text(text='Shail Patel ', pos=vp.vec(-7,-7,0),color=vp.color.cyan, billboard=True, emissive=True)
-
-
 
 
 vp.scene.width = 800
@@ -168,13 +123,10 @@ vp.scene.height = 800
 vp.scene.range = 1.3
 vp.scene.title = "ANTIKYTHERA\n"
 
-
 vp.button(text="display", bind=display, pos=vp.scene.title_anchor)
 vp.scene.append_to_title('\n')
 vp.button(text="display at", bind=showat, pos=vp.scene.title_anchor)
 vp.scene.append_to_title('\n')
-vp.button(text="credits", bind=credits, pos=vp.scene.title_anchor)
-
 vp.winput(bind=setday, pos=vp.scene.title_anchor)
 vp.winput(bind=setmonth, pos=vp.scene.title_anchor)
 vp.winput(bind=setyear, pos=vp.scene.title_anchor)
