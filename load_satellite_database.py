c2a63781a5163b87986808bf854b20e5b3ac4ec6
smithdepazd <44277887+smithdepazd@users.noreply.github.com>
2019-07-24 17:23:41 -0400

    Add files via upload

---
+++ load_satellite_database.py
@@ -0,0 +1,21 @@
+import sqlite3
+from orbitModeling import satellite
+
+
+def load_satellite_database():
+    # database file connection & setup
+    database = sqlite3.connect("Database")
+    cursor = database.cursor()
+
+    satellite_list = []
+
+    cursor.execute("""SELECT * FROM SATELLITE""")
+    query_result = cursor.fetchall()
+    print(query_result)
+    for i in query_result:
+        temp = satellite(i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8], i[9],i[10])
+        satellite_list.append(temp)
+
+    # function is READ-ONLY so no changes are made
+    database.close()
+    return satellite_list
